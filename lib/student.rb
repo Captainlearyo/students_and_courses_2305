e8908194fb4f89375f48c91b2263d8824fac1ae6
Captainlearyo <clayoleary@gmail.com>
2023-05-24 10:25:50 -0500

    complete student

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
 class Student
- 
+ attr_reader :name, :age, :scores
+ #attr__accessor :
+    def initialize(hash)
+      @name = hash[:name]
+      @age = hash[:age]
+      @scores = []
+
+
+    end  
+
+    def log_score(score)
+      @scores << score
+    end
+
+    def grade 
+      (@scores.reduce(0){ |sum, num| sum + num})/@scores.count.to_f
+    end
+
+
+
 end
